07096f163968580a54452da81fe9c4c16823e21f
beyanon <tokabe333@gmail.com>
2023-10-14 20:58:05 +0900

    update: hatena

--- ABC_300to350/ABC_323/E.rb
+++ ABC_300to350/ABC_323/E.rb
@@ -17,6 +17,22 @@ elsif xa == xb && xb == xc
 	end
 
 else   
-	
+	# 右上
+	if xb < xc && yb < yc 
+		mokuhyou = [[xb - 1, yb], [xb, yb - 1]]
+	# 右下
+	elsif xb < xc && yc < yb  
+		mokuhyou = [[xb - 1, yb], [xb, yb + 1]]
+	# 左下
+	elsif xc < xb && yc < yb 
+		mokuhyou = [[xb + 1, yb], [xb, yb + 1]]
+	# 右上
+	elsif xc < xb && yb < yc 
+		mokuhyou = [[xb + 1, yb], [xb, yb - 1]]
+	end
+end
 
+if mokuhyou.length > 1
+	d1 = (mokuhyou[0][0] - xa).abs + (mokuhyou[0][1] - ya).abs 
+	d2 = (mokuhyou[1][0] - xa).abs + (mokuhyou[])
 end
