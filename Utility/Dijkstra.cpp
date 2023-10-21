b5fb71c60249f78005e919d24dc15a157155036a
beyanon <tokabe333@gmail.com>
2023-10-22 01:01:00 +0900

    update : templateで実装

--- Utility/Dijkstra.cpp
+++ Utility/Dijkstra.cpp
@@ -1,46 +1,41 @@
 #include "../_CppTemplate.cpp"
 
-// 問題に合わせてバイト数を変更
-typedef long long            dijint;
-typedef pair<dijint, dijint> pdd;
-typedef vector<dijint>       vdi;
-typedef vector<pdd>          vpdd;
-typedef vector<vector<pdd>>  vvpdd;
-
 /// @brief ダイクストラ法である始点から全てのノードまでの距離を探索
-/// @param graph
-/// @param start
+/// @param graph vector<vector<pair<cost, node>>>
+/// @param start node
 /// @return 距離を列挙した配列
-vdi dijkstra(const vvpdd &graph, int start) {
+// 問題に合わせてバイト数を変更
+template <class T>
+vector<T> dijkstra(const vector<vector<pair<T, T>>> &graph, int start) {
     // 変数用意
     int n = graph.size();
 
     // とりあえず最大値
-    dijint MAX = sizeof(dijint) == 4 ? INT_MAX : LONG_MAX;
+    T MAX = sizeof(T) == 4 ? INT_MAX : LLONG_MAX;
 
     // 距離候補を保持する優先度付きキュー
-    priority_queue<pdd, vpdd, greater<pdd>> pque;
+    priority_queue<pair<T, T>, vector<pair<T, T>>, greater<pair<T, T>>> pque;
 
     // 確定した距離を保持するreturn用変数
-    vdi distance(n, MAX);
+    vector<T> distance(n, MAX);
     distance[start] = 0;
-    pque.push(pdd(0, start));
+    pque.push(pair<T, T>(0, start));
     while (pque.size() > 0) {
-        dijint d = pque.top().first;  // start から u までの距離
-        dijint u = pque.top().second; // 距離を知りたいノード
+        T d = pque.top().first;  // start から u までの距離
+        T u = pque.top().second; // 距離を知りたいノード
         pque.pop();
 
         // 既に確定した距離以上なら更新余地がない
         if (distance[u] < d) continue;
 
         // 各種距離を追加
-        for (pdd next : graph[u]) {
-            dijint cost      = next.first;
-            dijint next_node = next.second;
+        for (auto next : graph[u]) {
+            T cost      = next.first;
+            T next_node = next.second;
             // 更新余地がない場合は次
             if (distance[next_node] <= d + cost) continue;
             distance[next_node] = d + cost;
-            pque.push(pdd(d + cost, next_node));
+            pque.push(pair<T, T>(d + cost, next_node));
         } // end of for
     }     // end of while
 
